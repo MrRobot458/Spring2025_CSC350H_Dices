60e40e24e64bb06e1ccd3f2c62bc037ea62b3151
Emmanuel <erivas157@gmail.com>
2025-02-20 14:18:16 -0500

    Initial commit with .gitignore file

---
+++ DiceProgram/Dice.cs
@@ -0,0 +1,10 @@
+/*
+Name: Emmanuel Rivas
+ID: 15310887
+Class: Spring 2025, CSC 350H
+Instructor: Prof. Tang
+Date: 02/20/2025
+Dice Lab
+*/
+
+
---
+++ DiceProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Name: Emmanuel Rivas
+ID: 15310887
+Class: Spring 2025, CSC 350H
+Instructor: Prof. Tang
+Date: 02/20/2025
+Dice Lab
+*/
+
+
